2bf3489aa4d6a27c8118eecc29a4bdd7f776d7fb
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-10-21 20:41:29 +0300

    Update Tema4.py

--- Tema4.py
+++ Tema4.py
@@ -1,1 +1,23 @@
 
+def afis(p, val):
+    if(p>0):
+        print(val,end="")
+        return afis(p-1, val)
+    else:
+        return ""
+
+#print(afis(3,1))
+
+
+def triunghi(n):
+    def afis(p, val):
+        if (p > 0):
+            print(val, end=" ")
+            return afis(p - 1, val)
+        else:
+            print("\r")
+    if(n>0):
+        triunghi(n-1)
+    return afis(n,n)
+
+triunghi(5)
