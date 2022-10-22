f8e60b1ed648888a0baf72c097be5ff9f0b52435
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-10-22 17:17:00 +0300

    Update TEMA5.py

--- TEMA5.py
+++ TEMA5.py
@@ -65,4 +65,122 @@ print(lista)
   '''
   Să se înmulțească toate elementele unei liste de numere reale. Se va folosi funcția reduce().
   '''
+  
+import functools
+
+lista = [2.1, 3.5 , 4.0 , 5.5 , 6.33]
+rez=functools.reduce(lambda x,y: x*y , lista, 1)
+
+print(rez)
+
+#----------------------------------------------------------------------
+
+  #EX 6
+  
+  '''
+   Să se creeze o nouă listă care să conțină toate elementele unei liste inițiale la puterea a 3-a. Se va folosi funcția map().
+  '''
+
+lista = [2, 3, 4, 5, 6, 7 ,8]
+
+rez = map(lambda x: x**3, lista)
+print(list(rez))
+
+#-------------------------------------------------------------------------------
+
+#EX 7
+
+'''
+Să se creeze o nouă listă care conține doar elementele numere prime dintr-o listă inițială. Se va folosi funcția filter(). 
+'''
+
+lista = [2, 3, 4, 5, 6, 7 ,8]
+
+def prim(x):
+    def aux(n, i=2):
+        if(i > n/2):
+            return True
+        else:
+            if(n % i == 0):
+                return False
+            else:
+                return True and aux(n, i+1)
+    return aux(x)
+
+rez = filter(prim, lista)
+print(list(rez))
+
+#------------------------------------------------------------------------------------
+
+#P II
+
+  #EX 1 a)
+  '''
+  Implementați funcții care construiesc lista cifrelor unui număr care satisfac o condiție anume
+  (cifre impare, pare, mai mici decât 7, etc. la alegere), în ordine normală și inversă
+  '''
+  
+def cifre_a(n):
+    if(n < 10):
+        if(n % 2 == 1 and n < 7):
+            return [n]
+        else:
+            return []
+    else:
+        uc= n%10
+        if(uc % 2 ==1 and uc < 7):
+            return  cifre_a(n//10) + [uc]
+        else:
+            return cifre_a(n//10) + []
+
+
+print(cifre_a(1234567))
+
+# EX 1 b)
+'''
+ Implementați o funcție care construiește lista cifrelor unui număr care satisfac o condiție dată ca parametru
+ sub forma unei funcții cu tipul int -> bool.
+'''
+
+def cifre_conditie(n, conditie):
+    if n <= 9:
+        if conditie(n):
+            return [n]
+        else:
+            return []
+    else:
+        ultima_cifra = n % 10
+        if conditie(ultima_cifra):
+            return cifre_conditie(n // 10, conditie) + [ultima_cifra]
+        else:
+            return cifre_conditie(n // 10, conditie)
+
+
+print("lista cifre cond = ", end="")
+print(cifre_conditie(123469, lambda x: x % 3 == 0))
+
+#----------------------------------------------------------------------------
+
+#c
+'''
+Invers, dată fiind o listă de cifre, construiți numărul format doar din cifrele care respectă o condiție (dată ca parametru funcție cu tipul int -> bool).
+Rezolvați problema direct, recursiv, și apoi prin folosirea lui filter (selectarea cifrelor) cu reduce (pentru construirea numărului).
+'''
+
+lisa = [1,2,3,4,5,6,7,8,9]
+
+
+def cifre_c(lista, cond):
+    if(len(lista) > 0):
+        h=lista[-1]
+        t=lista[:-1]
+        if(cond(h)):
+            return h + cifre_c(t, cond)*10
+        else:
+            return cifre_c(t,cond)
+    else:
+        return 0
+
+print(cifre_c(lisa,lambda x: x%3==1)
+
 
