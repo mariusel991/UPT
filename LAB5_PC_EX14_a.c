cad661cfb629014ae1368f76e66feaad9b210db1
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-03 03:01:02 +0200

    Create LAB5_PC_EX14_a.c

---
+++ LAB5_PC_EX14_a.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+void bit(uint16_t n){
+  uint16_t mask;
+  mask = 1 << 15;
+  for(int i = 0; i < 16; i++){
+    if(n & mask){
+      printf("1");
+    }
+    else{
+      printf("0");
+    }
+    mask = mask >> 1;
+  }
+  printf("\n");
+}
+
+int main(){
+
+  uint16_t n,mask,nou=0x0000;
+  
+  scanf("%u", &n);
+  /// bit(n);
+  mask = 0xF000;
+  /// printf("0x%X\n", mask);
+  bit(n);
+  for(int i = 4; i >= 1 ; i--){
+    nou =nou|((n & mask) >> 4*(i-1));
+    n = n << 4;
+  }
+  bit(nou);
+
+  
+  
+  return 0;  
+}
