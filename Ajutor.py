794590f600e89316a2e193d56ff74f43e8f76d97
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-15 23:11:50 +0200

    Create Ajutor.py

---
+++ Ajutor.py
@@ -0,0 +1,10 @@
+def graf_serban(graf, lista):
+    if len(lista) > 0:
+        nod, nod_asociat = lista[0]
+        if nod_asociat not in graf[nod]:
+            graf[nod]+= [nod_asociat]
+        return  graf_serban(graf, lista[1:])
+    else:
+        return graf
+
+print(graf_serban({1:[2,3,4], 2:[1,3], 3:[1,4], 4:[1,3]} ,[(2,4), (3,2)]))
