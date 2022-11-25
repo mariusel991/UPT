85ff53ab00819890bafabb91fb8416bc6391b549
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-25 17:03:29 +0200

    Create LAB6_PC_EX1.c

---
+++ LAB6_PC/LAB6_PC_EX1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int prelucreaza(const char *str){
+    int k = 1;
+    for(int i = 0; i < strlen(str) - 1; i++){
+        ///printf("%d", k);
+        if(str[i] != str[i+1]){
+            k++;
+        }
+    }
+    return k;
+    
+}
+
+int main(){
+    
+    char v[32]="";
+    int rez;
+ 
+    if( fgets(v,32,stdin) == NULL){/// verificam daca se poate citi de la stdin
+        printf("error");
+        exit(1);
+    }
+    
+    rez = prelucreaza(v);
+    printf("%d\n", rez);
+    
+    
+    
+    
+}
