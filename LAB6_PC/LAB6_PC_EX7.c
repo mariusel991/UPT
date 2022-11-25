9e3c5cf7b1088d1b6d767295f7ea75512b030fa8
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-25 17:45:15 +0200

    Update LAB6_PC_EX7.c

--- LAB6_PC/LAB6_PC_EX7.c
+++ LAB6_PC/LAB6_PC_EX7.c
@@ -8,12 +8,13 @@
 int main(){
     
     int n, v[10];
-    
+    ///Citire elemente vector
     scanf("%d", &n);
     for(int i = 0; i < n; i++){///citim n elemente in vector
         scanf("%d", &v[i]);
     }
     
+    ///Prelucrae vector
     for(int i = 0, j = n-1; i < j;){/// parcurgem vectorul din extremitati spre mijloc
         if(v[i] % 2 == 0 && v[j] %2 == 1){///schimbam valorile pentru ca cele impare sa fie in fata si cele pare la urma
             int aux;
@@ -24,7 +25,7 @@ int main(){
         if(v[i] % 2 == 1) i++;/// nu am gasit valoare para in stanga
         if(v[j] % 2 == 0) j--;/// nu am gasit o valoare impara in dreapta
     }
-    
+    ///Afisare vector
     for(int i = 0; i < n; i++){/// afisam vectorul modificat
         printf("%d ", v[i]);
     }
