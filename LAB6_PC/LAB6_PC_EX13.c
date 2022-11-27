58aa5e7846b4239589dccbd244b435b5d59b86cf
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-27 19:09:54 +0200

    Update LAB6_PC_EX13.c

--- LAB6_PC/LAB6_PC_EX13.c
+++ LAB6_PC/LAB6_PC_EX13.c
@@ -1,3 +1,8 @@
+/*
+    13. Se citește un număr n<=10 și apoi n perechi de numere întregi. Se cere să se insereze după 
+    fiecare pereche din vectorul original suma și apoi produsul numerelor din pereche.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
