7c501f781af7754c875b0fb0a9dfa3aebc341ca2
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-27 21:15:39 +0200

    Update LAB7_PC_EX3.c

--- LAB7_PC/LAB7_PC_EX3.c
+++ LAB7_PC/LAB7_PC_EX3.c
@@ -1,1 +1,24 @@
+/*
+3.Să se citească de la tastatură două variabile de tip double (se citește cu %lf, nu cu %g!!!) și să se afișeze numele variabilei care se află la adresa 
+cea mai mică de memorie utilizând pointeri.
+*/
 
+
+#include <stdio.h>
+
+
+int main(){
+    double a, b;
+    
+    scanf("%lf %lf", &a, &b);
+   ///printf("%ld\n%ld\n", &a-&b, &b-&a);
+   ///printf("%p\n%p\n", &a, &b);
+    if(&a-&b > 0){
+        printf("a\n");
+    }
+    else{
+        printf("b\n");
+    }
+    
+    return 0;
+}
