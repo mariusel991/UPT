390f84eeb4d100c794e35907109df46bcee8582e
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-11-30 18:45:36 +0200

    Update LAB9_PC_EX7.c

--- LAB9/LAB9_PC_EX7.c
+++ LAB9/LAB9_PC_EX7.c
@@ -43,15 +43,49 @@ char *mystrchr(char *s, const int c){
     }
 }
 
+char *mystrstr(char* str, const char* strSub){
+    for(int i = 0; i < mystrlen(str) - mystrlen(strSub); i++){
+        int ok = 1;
+        for(int j = 0; j < mystrlen(strSub); j++){
+            if(*(str+i+j) != *(strSub+j)){
+                ok = 0;
+                break;
+            }
+        }
+        if(ok == 1) return (char *)(str+i);
+    }
+    return NULL;
+}
 
+int mystrcmp(const char *s1, const char *s2){
+    while(*s1 != '\0'){
+        if(*s1 != *s2){
+            break;
+        }
+        s1++;
+        s2++;
+    }
+    return (unsigned const char*)s1 - (unsigned const char* )s2;
+}
+
+char *mystrcat(char *dest, const char *src){
+    size_t ldest = mystrlen(dest);
+    for(int i = 0; i < mystrlen(src); i++){
+        *(dest + ldest + i) = *(src+i);
+    }
+    *(dest+mystrlen(dest)) = '\0';
+    return dest;
+}
 
 int main(){
-    char aux[20];
-    char s[]="ana";
+    char aux[20]="";
+    char s[]="ana are mere";
     
     mystrcpy(aux,s);
+    printf("%s\n", mystrstr(s,"r"));
+    strcat(aux,"are mere");
+    printf("%s\n", aux);
     
-    printf("%s\n",p);
     
     return 0;
 }
