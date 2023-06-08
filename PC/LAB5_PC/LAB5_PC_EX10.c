c35e829bd96f150cfbc5bdb98a86a404f1741293
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2023-06-08 18:07:08 +0300

    Update LAB5_PC_EX10.c

--- PC/LAB5_PC/LAB5_PC_EX10.c
+++ PC/LAB5_PC/LAB5_PC_EX10.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
 
+int main(){
+  int n;
+  uint32_t mask;
+  mask = 1 << 31;
+  // printf("%ld", sizeof(int));
+
+  scanf("%d", &n);
+
+  if(n & mask){
+    printf("1\n");
+  }
+  else{
+    printf("0\n");
+  }
+  
+  
+
+  return 0;
+  
+  
+}
